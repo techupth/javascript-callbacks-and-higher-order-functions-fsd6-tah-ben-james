f1f8f2a691009066fc42e1729ee401455309f2be
Your Name <you@example.com>
2024-05-17 13:21:17 +0700

    edit ex1-2 js.

--- ex-1.js
+++ ex-1.js
@@ -2,10 +2,19 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for (let i of employeeSalaries) {
+    newEmployeeSalaries.push(operation(i));
+  }
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
+
+function addmoney(money) {
+  return money + 5000;
+}
+
+let newSalary = forEach(employeeSalaries, addmoney);
 
 // Using `forEach` function here
 
--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,16 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0;
+  for (let i of array) {
+    if (operation(i)) {
+      count++;
+    }
+    if (count >= 5) {
+      return "ผ่านเกณฑ์ ✅";
+    }
+  }
+  return "ไม่ผ่านเกณฑ์ ❌";
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -9,7 +19,14 @@ const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
+function checkScore(score) {
+  return score > 70;
+}
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkScore);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkScore);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkScore);
+
+console.log(`นักเรียนห้องที่ 1 ${scoreRoom1Result}`);
+console.log(`นักเรียนห้องที่ 2 ${scoreRoom2Result}`);
+console.log(`นักเรียนห้องที่ 3 ${scoreRoom3Result}`);
